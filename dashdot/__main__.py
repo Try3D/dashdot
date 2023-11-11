e358eb1d847fc7803afa7d29c501c9f29c4731cb
try3d <leptonaray@gmail.com>
2023-11-11 21:27:47 +0530

    Add way to symlink files instead of folders

--- dashdot/__main__.py
+++ dashdot/__main__.py
@@ -40,50 +40,69 @@ def link_dotfiles(dotfiles_config):
     for section, settings in dotfiles_config.items():
         if section == "editor":
             continue
-        location = os.path.expandvars(settings.get("location", ""))
 
-        if not location:
+        locations = settings.get("location", [])
+
+        if not locations:
             print(f"Invalid configuration for '{section}'. Skipping.")
             continue
 
         dotfiles_path = os.path.join(os.path.abspath("."), section)
-        src_path = os.path.join(dotfiles_path)
-        dst_path = os.path.expandvars(location)
+        if isinstance(locations, str):
+            src_path = os.path.join(dotfiles_path)
+            dst_path = os.path.expandvars(locations)
 
-        try:
-            os.symlink(src_path, dst_path)
-            print(f"Symlink created: {dst_path}")
-        except FileExistsError:
-            print(f"Symlink already exists: {dst_path}")
+            try:
+                os.symlink(src_path, dst_path)
+                print(f"Symlink created: {dst_path}")
+            except FileExistsError:
+                print(f"Symlink already exists: {dst_path}")
+        elif isinstance(locations, list):
+            for location in locations:
+                src_path = os.path.join(dotfiles_path, location["src"])
+                dst_path = os.path.expandvars(location["dest"])
+
+                try:
+                    os.symlink(src_path, dst_path)
+                    print(f"Symlink created: {dst_path}")
+                except FileExistsError:
+                    print(f"Symlink already exists: {dst_path}")
 
 
 def delink_dotfiles(dotfiles_config):
     for section, settings in dotfiles_config.items():
         if section == "editor":
             continue
-        location = os.path.expandvars(settings.get("location", ""))
+        locations = settings.get("location", [])
 
-        if not location:
+        if not locations:
             print(f"Invalid configuration for '{section}'")
             continue
 
-        dst_path = os.path.expandvars(location)
+        if isinstance(locations, str):
+            dst_path = os.path.expandvars(locations)
 
-        try:
-            os.unlink(dst_path)
-            print(f"Symlink deleted: {dst_path}")
-        except FileNotFoundError:
-            print(f"Symlink not found: {dst_path}")
+            try:
+                os.unlink(dst_path)
+                print(f"Symlink deleted: {dst_path}")
+            except FileNotFoundError:
+                print(f"Symlink not found: {dst_path}")
+        elif isinstance(locations, list):
+            for location in locations:
+                dst_path = os.path.expandvars(location["dest"])
+                try:
+                    os.unlink(dst_path)
+                    print(f"Symlink deleted: {dst_path}")
+                except FileNotFoundError:
+                    print(f"Symlink not found: {dst_path}")
 
 
 def edit_dotfiles(dotfiles_config, config_to_edit=None):
     if config_to_edit and config_to_edit in dotfiles_config:
         editor = dotfiles_config.get("editor", "nano")
-        location = os.path.expandvars(
-            dotfiles_config[config_to_edit].get("location", ""))
         main_file = dotfiles_config[config_to_edit].get("main", "")
 
-        if location and main_file:
+        if main_file:
             dotfiles_path = os.path.join(os.path.abspath("."), config_to_edit)
             src_path = os.path.join(dotfiles_path, main_file)
             try:
